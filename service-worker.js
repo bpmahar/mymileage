c6815f71d946716765a189971ff22b2626aa9293
B P Meena <bpmeena@gmail.com>
2026-03-16 21:07:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,32 @@
-const CACHE_NAME = "my-mileage-v2";
+const CACHE_NAME = "mymileage-cache-v2";
 
-const FILES_TO_CACHE = [
-  "./",
+const urlsToCache = [
+  "/",
   "index.html",
-  "manifest.json"
+  "manifest.json",
+  "icon.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -20,4 +35,11 @@ self.addEventListener("fetch", event => {
       return response || fetch(event.request);
     })
   );
+});
+
+/* listen for update message */
+self.addEventListener("message", event => {
+  if (event.data === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
