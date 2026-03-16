118e70aabebc85753f905a1b9ba138296fadb99c
B P Meena <bpmeena@gmail.com>
2026-03-16 21:13:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,6 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
